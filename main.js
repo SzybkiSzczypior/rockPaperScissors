2cf5bb87c88076d5c452d73c1dda93f42008063c
SzybkiSzczypior <kalitar693@gmail.com>
2023-07-13 17:18:38 +0200

    --

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const r = document.getElementById("r");
-const p = document.getElementById("p");
-const s = document.getElementById("s");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
 
-const playerInput = [r,p,s];
-const computerInput = ["r"]
+const playerInput = [rock,paper,scissors];
+
 //document.getElementById("r").addEventListener("click",function () {
    // alert("hello");
   //  console.log("hello");
@@ -12,11 +12,53 @@ const computerInput = ["r"]
 
 
 playerInput.forEach(input => input.addEventListener("click",(e) => {
-    const showInput = document.getElementById("showInput");
+    //const showInput = document.getElementById("showInput");
     const rps1 = e.target.id; 
-    showInput.innerHTML= rps1;
+  //  showInput.innerHTML= rps1;
+    const computerInput = ['rock','paper','scissors'];
+   
+   const random = Math.floor(Math.random() * 3);
+    
+    const computerChoice = computerInput[random];
+   console.log(computerChoice);
+   const sw = document.getElementById("showWinner")
+    if (computerChoice == rps1) {
+       
+          sw.textContent = "draw";
+    }
+    else if(computerChoice == 'rock') {
+        if (rps1 == 'scissors') {
+            sw.textContent= "Computer won";         
+        } else {
+            sw.textContent= "Player lost"; 
 
+            
+        }
+        
+        
+    }else if (computerChoice== 'scissors') {
+        if (rps1 == 'paper') {
+            sw.textContent= "Computer won";
+            
+        }else{
+            sw.textContent= "Player lost";
+        }
+    }else if (computerChoice == 'paper') {
+        if (rps1 == 'rock') {
+            sw.textContent= "Computer won";
+        }else {
+            sw.textContent= "Player lost";
+        }
+        
+    }
+      
+    
 }))
+
+
+
+
+
 
 
 
